151f5f8c04d3fc0154dd488a609c262ec8b16d25
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-01 19:47:46 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,14 @@ Rails.application.routes.draw do
 
   get 'admin/settings'
 
+  get 'admin/dashboard'
+
+  get 'admin/stats'
+
+  get 'admin/financials'
+
+  get 'admin/settings'
+
   get 'categories/show'
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
